3bc96c002caee788ccb776688fe050f7bf2665f7
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2020-12-15 14:03:38 -0600

    first call post api v6

--- constants/constants.js
+++ constants/constants.js
@@ -5,6 +5,7 @@ const GLOBAL_CONSTANTS = {
   PASS_DATABASE: "Hfy2020Db#",
   SERVER_DATABASE: "hfy-database-1.cuvtxvydqsnt.us-east-2.rds.amazonaws.com",
   DATABASE_NAME: "homifysys",
+  DATABASE_PORT: 1433,
 };
 
 module.exports = GLOBAL_CONSTANTS;
