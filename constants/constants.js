1ded7b28aae8a65388df69e895f7a9bfdef7790a
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2020-12-14 20:12:08 -0600

    first version basic backend

---
+++ constants/constants.js
@@ -0,0 +1,10 @@
+const GLOBAL_CONSTANTS = {
+  VERSION: "v0.0.1",
+  PORT: 3000,
+  USER_DATABASE: "admin",
+  PASS_DATABASE: "Hfy2020Db#",
+  SERVER_DATABASE: "hfy-database-1.cuvtxvydqsnt.us-east-2.rds.amazonaws.com",
+  DATABASE_NAME: "homifysys",
+};
+
+module.exports = GLOBAL_CONSTANTS;
