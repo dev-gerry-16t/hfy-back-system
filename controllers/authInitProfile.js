5a4057f35bc1ab6350bea7823840102c56c887f2
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2020-12-29 16:04:05 -0600

    add new api getCustomerId and baerer repllace

--- controllers/authInitProfile.js
+++ controllers/authInitProfile.js
@@ -47,6 +47,36 @@ const executeMenuUserProfile = async (params, res) => {
   }
 };
 
+const executeGetCustomerById = async (params, res) => {
+  const {
+    idCustomer,
+    idSystemUser,
+    idLoginHistory,
+    offset = "-06:00",
+  } = params;
+  try {
+    const request = new sql.Request();
+    request.input("p_nvcIdCustomer", sql.NVarChar, idCustomer);
+    request.input("p_nvcIdSystemUser", sql.NVarChar, idSystemUser);
+    request.input("p_nvcIdLoginHistory", sql.NVarChar, idLoginHistory);
+    request.input("p_chrOffset", sql.Char, offset);
+    request.execute("customerSch.USPgetCustomerById", (err, result) => {
+      if (err) {
+        res.status(500).send({ response: "Error en los parametros" });
+      } else {
+        const resultRecordset = result.recordset;
+        result;
+        res.status(200).send({
+          response: resultRecordset,
+        });
+      }
+    });
+  } catch (err) {
+    console.log("ERROR", err);
+    // ... error checks
+  }
+};
+
 const ControllerAuth = {
   userProfile: (req, res) => {
     const params = req.body;
@@ -55,6 +85,10 @@ const ControllerAuth = {
   userMenuProfile: (req, res) => {
     const params = req.body;
     executeMenuUserProfile(params, res);
+  },
+  getCustomerById: (req, res) => {
+    const params = req.body;
+    executeGetCustomerById(params, res);
   },
 };
 
