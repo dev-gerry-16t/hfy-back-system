450286eefc2d08a1e4ab2c5b06ae156b94ca149f
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-01-14 13:00:06 -0600

    documents

--- controllers/repositoryDocuments.js
+++ controllers/repositoryDocuments.js
@@ -1,6 +1,13 @@
 const sql = require("mssql");
+const AWS = require("aws-sdk");
+const GLOBAL_CONSTANTS = require("../constants/constants");
 
-const executeAddDocument = async (params, res) => {
+const s3 = new AWS.S3({
+  accessKeyId: "AKIAJZ2VBYROHPNFRTKA",
+  secretAccessKey: "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",
+});
+
+const executeAddDocument = async (params, res, file) => {
   const {
     idCustomer,
     idSystemUser,
@@ -12,13 +19,15 @@ const executeAddDocument = async (params, res) => {
     thumbnail,
     idDocumentType,
   } = params;
+  const fileName = documentName.split(".");
+  const fileType = fileName[fileName.length - 1];
   try {
     const request = new sql.Request();
     request.input("p_nvcIdCustomer", sql.NVarChar, idCustomer);
     request.input("p_nvcIdSystemUser", sql.NVarChar, idSystemUser);
     request.input("p_nvcIdLoginHistory", sql.NVarChar, idLoginHistory);
     request.input("p_nvcDocumentName", sql.NVarChar, documentName);
-    request.input("p_nvcExtension", sql.NVarChar, extension);
+    request.input("p_nvcExtension", sql.NVarChar, fileType);
     request.input("p_nvcPreview", sql.NVarChar, preview);
     request.input("p_nvcThumbnail", sql.NVarChar, thumbnail);
     request.input("p_chrOffset", sql.Char, offset);
@@ -33,8 +42,18 @@ const executeAddDocument = async (params, res) => {
             response: resultRecordset[0],
           });
         } else {
-          res.status(200).send({
-            response: resultRecordset[0],
+          const bucketSorce = resultRecordset[0].bucketSource.toLowerCase();
+          const idDocument = resultRecordset[0].idDocument;
+          const params = {
+            Bucket: bucketSorce,
+            Key: idDocument,
+            Body: file.buffer,
+          };
+          s3.upload(params, (err, data) => {
+            if (err) throw err;
+            res.status(200).send({
+              response: resultRecordset[0],
+            });
           });
         }
       }
@@ -47,8 +66,9 @@ const executeAddDocument = async (params, res) => {
 
 const ControllerDocuments = {
   addDocument: (req, res) => {
-    const params = req.body;
-    executeAddDocument(params, res);
+    const params = JSON.parse(req.body.fileProperties);
+    const fileParams = req.file;
+    executeAddDocument(params, res, fileParams);
   },
 };
 
