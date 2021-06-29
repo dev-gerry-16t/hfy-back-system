b7a299aafc20d885e2e54247fdab5161320e0909
dev-gerry-16t <69758055+dev-gerry-16t@users.noreply.github.com>
2021-06-28 23:36:10 -0500

    add dispersion order 2

--- actions/dispersionOrder.js
+++ actions/dispersionOrder.js
@@ -12,70 +12,70 @@ const executeGetDispersionOrder = async (params, res) => {
       .input("p_chrOffset", sql.Char, offset)
       .execute("stpSch.USPgetDispersionOrder");
     const resultRecordset = result.recordset;
-    // for (const element of resultRecordset) {
-    //   const {
-    //     idDispersionOrder,
-    //     institucionContraparte,
-    //     empresa,
-    //     fechaOperacion,
-    //     claveRastreo,
-    //     institucionOperante,
-    //     monto,
-    //     tipoPago,
-    //     tipoCuentaOrdenante,
-    //     nombreOrdenante,
-    //     cuentaOrdenante,
-    //     rfcCurpOrdenante,
-    //     tipoCuentaBeneficiario,
-    //     nombreBeneficiario,
-    //     cuentaBeneficiario,
-    //     rfcCurpBeneficiario,
-    //     conceptoPago,
-    //     referenciaNumerica,
-    //   } = element;
-    //   const bodyRequest = {
-    //     institucionContraparte: 846,
-    //     empresa,
-    //     fechaOperacion,
-    //     claveRastreo,
-    //     institucionOperante,
-    //     monto,
-    //     tipoPago,
-    //     tipoCuentaOrdenante,
-    //     nombreOrdenante,
-    //     cuentaOrdenante,
-    //     rfcCurpOrdenante,
-    //     tipoCuentaBeneficiario,
-    //     nombreBeneficiario,
-    //     cuentaBeneficiario: "846180000000000016",
-    //     rfcCurpBeneficiario,
-    //     conceptoPago,
-    //     referenciaNumerica,
-    //   };
-    //   const crypto = new CryptoHandler(
-    //     bodyRequest,
-    //     GLOBAL_CONSTANTS.SECRET_KEY_ENCRYPT,
-    //     null
-    //   );
-    //   const orderPay = { ...bodyRequest, firma: crypto.getSign() };
-    //   //console.log("orderPay", JSON.stringify(orderPay, null, 2));
-    //   const response = await rp({
-    //     url: "https://demo.stpmex.com:7024/speiws/rest/ordenPago/registra",
-    //     method: "PUT",
-    //     headers: {
-    //       encoding: "UTF-8",
-    //       "Content-Type": "application/json",
-    //     },
-    //     json: true,
-    //     body: orderPay,
-    //     rejectUnauthorized: false,
-    //   });
-    //   //console.log("response", JSON.stringify(response, null, 2));
-    //   await executeSetDispersionOrder({
-    //     idDispersionOrder,
-    //     jsonServiceResponse: JSON.stringify(response),
-    //   });
-    // }
+    for (const element of resultRecordset) {
+      const {
+        idDispersionOrder,
+        institucionContraparte,
+        empresa,
+        fechaOperacion,
+        claveRastreo,
+        institucionOperante,
+        monto,
+        tipoPago,
+        tipoCuentaOrdenante,
+        nombreOrdenante,
+        cuentaOrdenante,
+        rfcCurpOrdenante,
+        tipoCuentaBeneficiario,
+        nombreBeneficiario,
+        cuentaBeneficiario,
+        rfcCurpBeneficiario,
+        conceptoPago,
+        referenciaNumerica,
+      } = element;
+      const bodyRequest = {
+        institucionContraparte: 846,
+        empresa,
+        fechaOperacion,
+        claveRastreo,
+        institucionOperante,
+        monto,
+        tipoPago,
+        tipoCuentaOrdenante,
+        nombreOrdenante,
+        cuentaOrdenante,
+        rfcCurpOrdenante,
+        tipoCuentaBeneficiario,
+        nombreBeneficiario,
+        cuentaBeneficiario: "846180000000000016",
+        rfcCurpBeneficiario,
+        conceptoPago,
+        referenciaNumerica,
+      };
+      const crypto = new CryptoHandler(
+        bodyRequest,
+        GLOBAL_CONSTANTS.SECRET_KEY_ENCRYPT,
+        null
+      );
+      const orderPay = { ...bodyRequest, firma: crypto.getSign() };
+      //console.log("orderPay", JSON.stringify(orderPay, null, 2));
+      const response = await rp({
+        url: "https://demo.stpmex.com:7024/speiws/rest/ordenPago/registra",
+        method: "PUT",
+        headers: {
+          encoding: "UTF-8",
+          "Content-Type": "application/json",
+        },
+        json: true,
+        body: orderPay,
+        rejectUnauthorized: false,
+      });
+      //console.log("response", JSON.stringify(response, null, 2));
+      await executeSetDispersionOrder({
+        idDispersionOrder,
+        jsonServiceResponse: JSON.stringify(response),
+      });
+    }
   } catch (err) {
     throw err;
   }
